86d20b05a0a4994e7799eeab8868d3c4e99943e2
Marcel-Maz <marcel.mazeewski@hs-osnabrueck.de>
2022-11-07 20:09:21 +0100

    SMQ

--- aufgabe2/Consumer.java
+++ aufgabe2/Consumer.java
@@ -1,5 +1,17 @@
 package aufgabe2;
 
-public class Consumer {
-    
+public class Consumer<T> {
+    public void getMessage(String nameQ, SMQ<T> smq){
+        smq.retrieveMessage(nameQ, this);
+    }
+
+    public void startListen(String nameQ, SMQ<T> smq){
+        if(!smq.addListener(nameQ, this)){
+            System.out.println("Hinzufügen fehlgeschlagen"); 
+        }
+    }
+
+    public void receiveMessage(T message){
+        System.out.println(message);
+    }
 }
--- aufgabe2/Producer.java
+++ aufgabe2/Producer.java
@@ -2,4 +2,14 @@ package aufgabe2;
 
 public class Producer {
     
+    public void sendMessage(String nameQ, String message, SMQ<String> smq){
+
+            smq.addMessage(nameQ,message, false, false);
+
+    }
+    public void sendMessage(String nameQ, String message, SMQ<String> smq, boolean fixedCapacity, boolean discarding){
+
+        smq.addMessage(nameQ, message, fixedCapacity, discarding);
+
+}
 }
